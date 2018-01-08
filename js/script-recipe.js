9f3bcac0151771c8a268c7bb02273f799cafa3ad
bimiriya <poyarzunaspee@gmail.com>
2018-01-08 17:00:31 -0300

    etapa 8: funciones para ocultar y mostrar pestañas make y recipe en recipe.html listas

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
    console.log("tamo ready")
+
+   $(".js-show-recipe").click(() => {
+       $("#page").removeClass("make")
+   })
+
+   $(".js-show-make").click(() => {
+       $("#page").addClass("make")
+   })
 })
